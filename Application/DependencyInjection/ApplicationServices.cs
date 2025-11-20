5961e9deb4e9e5810583a1d5067a69f3c8059cfd
Trần Thanh Bình <tranthanhbinh.thcsthanhson@gmail.com>
2025-11-20 19:16:11 +0700

    fix: not DI in MediatR

---
+++ Application/DependencyInjection/ApplicationServices.cs
@@ -0,0 +1,24 @@
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application.DependencyInjection;
+
+public static class ApplicationServices
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(assembly);
+        });
+
+        services.AddValidatorsFromAssembly(assembly);
+
+        services.AddTransient(typeof(MediatR.IPipelineBehavior<,>), typeof(Behaviors.ValidationBehavior<,>));
+
+        return services;
+    }
+}
