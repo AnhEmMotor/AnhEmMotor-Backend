dbac4034088d05a5d8a6c5bce2fd90747cb1b853
Trần Thanh Bình <tranthanhbinh.thcsthanhson@gmail.com>
2026-01-09 11:24:27 +0700

    fix: correct fluent assertions copyright display

---
+++ ControllerTests/FluentAssertionsConfig.cs
@@ -0,0 +1,18 @@
+﻿using ControllerTests;
+using FluentAssertions;
+using FluentAssertions.Extensibility;
+
+[assembly: AssertionEngineInitializer(
+    typeof(AssertionEngineConfiguration),
+    nameof(AssertionEngineConfiguration.AcknowledgeSoftWarning))]
+
+namespace ControllerTests
+{
+    public static class AssertionEngineConfiguration
+    {
+        public static void AcknowledgeSoftWarning()
+        {
+            License.Accepted = true;
+        }
+    }
+}
---
+++ IntegrationTests/FluentAssertionsConfig.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using FluentAssertions.Extensibility;
+using IntegrationTests;
+
+[assembly: AssertionEngineInitializer(
+    typeof(AssertionEngineConfiguration),
+    nameof(AssertionEngineConfiguration.AcknowledgeSoftWarning))]
+
+namespace IntegrationTests
+{
+    public static class AssertionEngineConfiguration
+    {
+        public static void AcknowledgeSoftWarning()
+        {
+            License.Accepted = true;
+        }
+    }
+}
