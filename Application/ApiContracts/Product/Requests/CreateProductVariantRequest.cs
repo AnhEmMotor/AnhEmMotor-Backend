0d3e159e526ecd1af4f245f14732910d1a2f6657
Trần Thanh Bình <tranthanhbinh.thcsthanhson@gmail.com>
2026-02-04 20:56:30 +0700

    feat: fix some error + add product category authen

--- Application/ApiContracts/Product/Requests/CreateProductVariantRequest.cs
+++ Application/ApiContracts/Product/Requests/CreateProductVariantRequest.cs
@@ -5,17 +5,13 @@ namespace Application.ApiContracts.Product.Requests;
 public class CreateProductVariantRequest
 {
     public decimal? Price { get; set; }
-
-    [JsonPropertyName("url")]
+    
     public string? UrlSlug { get; set; }
 
-    [JsonPropertyName("cover_image_url")]
     public string? CoverImageUrl { get; set; }
 
-    [JsonPropertyName("photo_collection")]
     public List<string> PhotoCollection { get; set; } = [];
 
-    [JsonPropertyName("optionValues")]
     public Dictionary<string, string> OptionValues { get; set; } = [];
 
     [JsonIgnore]
