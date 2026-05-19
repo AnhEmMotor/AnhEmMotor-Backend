90155b0def75759ce292583e38d2958bc3f61af9
Trần Thanh Bình <tranthanhbinh.thcsthanhson@gmail.com>
2026-05-19 15:09:18 +0700

    feat: add filter for brand of product

--- Application/Sieve/CustomSieveProcessor.cs
+++ Application/Sieve/CustomSieveProcessor.cs
@@ -29,7 +29,8 @@ public class CustomSieveProcessor(IOptions<SieveOptions> options) : SieveProcess
         mapper.Property<Product>(p => p.Id).CanSort().CanFilter();
         mapper.Property<Product>(p => p.Name).CanSort().CanFilter();
         mapper.Property<Product>(p => p.BrandId).CanFilter();
-        mapper.Property<Product>(p => p.CategoryId).CanFilter();
+        mapper.Property<Product>(p => p.CategoryId).CanFilter().HasName("category_id");
+        mapper.Property<Product>(p => p.Brand!.Name).CanFilter().HasName("brand");
         mapper.Property<Product>(p => p.StatusId).CanFilter();
         mapper.Property<Product>(p => p.StdDot).CanFilter();
         mapper.Property<Product>(p => p.StdEce).CanFilter();
