8000094cc390d10994cb89f21636610722e0cc76
Trần Thanh Bình <tranthanhbinh.thcsthanhson@gmail.com>
2026-05-27 09:03:36 +0700

    feat: create QuotationType enum and Quotation table for Quotation feature

---
+++ Domain/Entities/Quotation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Domain.Entities
+{
+    public class Quotation : BaseEntity
+    {
+        [Key]
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Column("Code", TypeName = "nvarchar(100)")]
+        public string? Code { get; set; }
+
+        [Column("SupplierId")]
+        [ForeignKey("Supplier")]
+        public int? SupplierId { get; set; }
+
+        [Column("Status", TypeName = "varchar(30)")]
+        public string? Status { get; set; }
+
+        [Column("Notes", TypeName = "nvarchar(MAX)")]
+        public string? Notes { get; set; }
+
+        public ICollection<QuotationProductRow> QuotationProductRows { get; set; } = [];
+
+        public Supplier? Supplier { get; set; }
+    }
+}
---
+++ Domain/Entities/QuotationProductRow.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Domain.Entities
+{
+    public class QuotationProductRow
+    {
+        [Key]
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Column("QuotationId")]
+        [ForeignKey("QuotationReceipt")]
+        public int? QuotationId { get; set; }
+
+        [Column("ProductVariantId")]
+        [ForeignKey("ProductVariant")]
+        public int? ProductVariantId { get; set; }
+
+        [Column("ProductVariantColorId")]
+        [ForeignKey("ProductVariantColor")]
+        public int? ProductVariantColorId { get; set; }
+
+        [Column("QuotePrice")]
+        public int? QuotePrice { get; set; }
+
+        public Quotation? QuotationReceipt { get; set; }
+
+        public ProductVariant? ProductVariant { get; set; }
+
+        public ProductVariantColor? ProductVariantColor { get; set; }
+    }
+}
