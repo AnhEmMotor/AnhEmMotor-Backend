5edeb7369d25342f2422da551d0d76fd6640dcb3
Kim Ngân <kimnganlqdqt@gmail.com>
2026-07-27 19:12:56 +0700

    pub
    pub code 27/07

--- Infrastructure/Persistence/ApplicationDbContextFactory.cs
+++ Infrastructure/Persistence/ApplicationDbContextFactory.cs
@@ -1,29 +1,13 @@
 using Infrastructure.DBContexts;
 using Microsoft.EntityFrameworkCore.Design;
 
-namespace Infrastructure.Persistence
+namespace Infrastructure.Persistence;
+
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDBContext>
 {
-    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDBContext>
+    public ApplicationDBContext CreateDbContext(string[] args)
     {
-        public ApplicationDBContext CreateDbContext(string[] args)
-        {
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine(
-                "=========================================================================================");
-            Console.WriteLine("CẢNH BÁO: Không được tạo migration trực tiếp bằng ApplicationDBContext!");
-            Console.WriteLine("Bạn bắt buộc phải sử dụng một trong các DbContext cụ thể sau:");
-            Console.WriteLine(" - SqlServerDBContext");
-            Console.WriteLine(" - MySqlDbContext");
-            Console.WriteLine(" - PostgreSqlDbContext");
-            Console.WriteLine();
-            Console.WriteLine("Ví dụ command:");
-            Console.WriteLine(
-                "dotnet ef migrations add <MigrationName> --context SqlServerDBContext --output-dir Migrations/SqlServerMigrations");
-            Console.WriteLine(
-                "=========================================================================================");
-            Console.ResetColor();
-            throw new InvalidOperationException(
-                "Cannot create migration using ApplicationDBContext. Please use a specific provider DbContext (SqlServerDBContext, MySqlDbContext, or PostgreSqlDbContext).");
-        }
+        throw new InvalidOperationException(
+            "Cannot create migration using ApplicationDBContext. Please use a specific provider DbContext (SqlServerDBContext, MySqlDbContext, or PostgreSqlDbContext).");
     }
 }
