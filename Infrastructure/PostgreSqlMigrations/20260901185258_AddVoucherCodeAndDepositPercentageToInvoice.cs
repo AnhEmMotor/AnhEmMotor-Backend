40b1d33b3d59354f6d5612dd66f1c7491b1846ad
Kim Ngân <kimnganlqdqt@gmail.com>
2026-09-02 01:53:13 +0700

    chore: add ef core migration for PostgreSQL to add VoucherCode and DepositPercentage to Invoice

---
+++ Infrastructure/PostgreSqlMigrations/20260901185258_AddVoucherCodeAndDepositPercentageToInvoice.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.PostgreSqlMigrations
+{
+    /// <inheritdoc />
+    public partial class AddVoucherCodeAndDepositPercentageToInvoice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DepositPercentage",
+                table: "Invoice",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "VoucherCode",
+                table: "Invoice",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DepositPercentage",
+                table: "Invoice");
+
+            migrationBuilder.DropColumn(
+                name: "VoucherCode",
+                table: "Invoice");
+        }
+    }
+}
